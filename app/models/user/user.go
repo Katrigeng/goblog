066e310df7191cbcb44e90015496a0f14dda7bdc
katrigeng <453875798@qq.com>
2023-12-14 11:53:40 +0800

    注册表单验证

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -5,7 +5,8 @@ import "goblog/app/models"
 type User struct {
 	models.BaseModel
 
-	Name     string `gorm:"column:name;type:varchar(255);not null;unique"`
-	Email    string `gorm:"column:email;type:varchar(255);default:NULL;unique;"`
-	Password string `gorm:"column:password;type:varchar(255)"`
+	Name            string `gorm:"type:varchar(255);not null;unique"`
+	Email           string `gorm:"type:varchar(255);unique;"`
+	Password        string `gorm:"type:varchar(255)"`
+	PasswordConfirm string `gorm:"-" valid:"password_confirm"`
 }
