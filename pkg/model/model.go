a3fbf9dbfceea3586c3df82787571069dfcb50bc
katrigeng <453875798@qq.com>
2023-12-28 14:24:31 +0800

    配置和环境变量

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"fmt"
+	"goblog/pkg/config"
 	"goblog/pkg/logger"
 
 	"gorm.io/gorm"
@@ -18,14 +20,28 @@ func ConnectDB() *gorm.DB {
 
 	var err error
 
-	config := mysql.New(mysql.Config{
+	gormConfig := mysql.New(mysql.Config{
 		// DSN: "root:123456@tcp(192.168.240.182:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
-		DSN: "root:root@tcp(127.0.0.1:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+		// DSN: "root:root@tcp(127.0.0.1:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+		DSN: fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=%v&parseTime=True&loc=Local",
+			config.GetString("database.username"),
+			config.GetString("database.password"),
+			config.GetString("database.host"),
+			config.GetString("database.port"),
+			config.GetString("database.database"),
+			config.GetString("database.charset")),
 	})
 
+	var level gormlogger.LogLevel
+	if config.GetBool("app.debug") {
+		level = gormlogger.Warn
+	} else {
+		level = gormlogger.Error
+	}
+
 	// 准备数据库连接池
-	DB, err = gorm.Open(config, &gorm.Config{
-		Logger: gormlogger.Default.LogMode(gormlogger.Warn),
+	DB, err = gorm.Open(gormConfig, &gorm.Config{
+		Logger: gormlogger.Default.LogMode(level),
 	})
 
 	logger.LogError(err)
